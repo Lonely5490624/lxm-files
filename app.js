da7809784dda7cdc4be1f00a4f80779d478f583a
lilong <l5490624@126.com>
2020-01-09 13:45:23 +0800

    设置上传文件大小为200mb

--- app.js
+++ app.js
@@ -33,10 +33,10 @@ app.all('*', function(req, res, next) {
 })
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '200mb' }));
+app.use(express.urlencoded({ limit: '200mb', extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'files')));
 app.use(fileupload())
 
 app.use(checkToken)
